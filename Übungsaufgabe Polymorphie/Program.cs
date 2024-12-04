185c69ccd19d062642644968f36430809590a4fe
Kuscheltiger01 <98894446+Kuscheltiger01@users.noreply.github.com>
2024-12-04 09:57:55 +0100

    Main program for Polymorphieübung

---
+++ Übungsaufgabe Polymorphie/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Übungsaufgabe_Polymorphie;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            _name = "Joe Unemployed",
+            _einkommen = 6400
+        };
+
+        Arbeiter arbeiter = new Arbeiter
+        {
+            _name = "Susi Hard Working",
+            _einkommen = 33600
+        };
+
+        Banker banker = new Banker
+        {
+            _name = "Fred Moneymaker",
+            _einkommen = 4000000
+        };
+
+        Königin königin = new Königin
+        {
+            _name = "Elizabeth",
+            _einkommen = 1000000
+        };
+
+        Person[] personenliste = new[] { person, arbeiter, banker, königin }; 
+        
+        double gesamteSteuer = Finanzamt.berechneGesamteSteuerEinnahmen(personenliste);
+        
+        Console.WriteLine($"Gesamte Steuereinnahmen: {gesamteSteuer}");
+    }
+}
