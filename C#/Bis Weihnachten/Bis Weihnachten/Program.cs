80bcca0d7905aaca3ff0b8bd7937a36d2a86d0cd
Louis <louis.ochocki@gmail.com>
2025-01-17 11:49:35 +0100

    test
    test

---
+++ C#/Bis Weihnachten/Bis Weihnachten/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Design;
+
+Console.Write("Noch wie viele Tage bist Weihnachten: ");
+
+for (int Tage = Convert.ToInt32(Console.ReadLine()); Tage >= 0; Tage--)
+{
+    if (Tage == 10)
+        Console.WriteLine($"Noch {Tage} Tage bis Weihnachten - Fast da!");
+    else if (Tage == 5)
+        Console.WriteLine($"Noch {Tage} Tage bis Weihnachten - Bereitmachen!");
+    else if (Tage == 1)
+        Console.WriteLine($"Noch {Tage} Tage bis Weihnachten - Morgen ist es soweit!");
+    else if (Tage == 0)
+        Console.WriteLine("Es ist Weihnachten");
+    else
+        Console.WriteLine($"Noch {Tage} Tage bis Weihnachten");
+}
---
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{i + 1} Hello, World!");
+                
+            }
+        }
+    }
+}
