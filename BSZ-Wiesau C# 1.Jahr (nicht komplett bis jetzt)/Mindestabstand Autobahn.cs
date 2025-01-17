d9451958b07684cfebcafb768c4b5d26de251839
Louis <louis.ochocki@gmail.com>
2025-01-17 10:56:02 +0100

    1.Lehrjahr
    Hinzufügen einiger Projekte aus dem 1.Lehrjahr

---
+++ BSZ-Wiesau C# 1.Jahr (nicht komplett bis jetzt)/Mindestabstand Autobahn.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Wie schnell fahren Sie (km/h): ");
+double geschwindigkeit = Convert.ToDouble(Console.ReadLine());
+
+double mindestabstand;
+
+mindestabstand = geschwindigkeit / 2;
+
+Console.WriteLine(mindestabstand + " m");
+
+double Reaktionsweg;
+double Bremsweg;
+
+Reaktionsweg = geschwindigkeit * 3 / 10;
+Bremsweg = (geschwindigkeit / 10) * (geschwindigkeit / 10);
+
+Console.WriteLine(Reaktionsweg + Bremsweg);
---
+++ BSZ-Wiesau C# 1.Jahr (nicht komplett bis jetzt)/Rechner.cs
@@ -0,0 +1,47 @@
+﻿double ergebnis;
+
+Console.Write("Gib Zahl 1 ein: ");
+double Zahl1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Gib Zahl 2 ein: ");
+double Zahl2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Wo mit wollen sie rechnen? + - * / % ('a' für alle rechnungen): ");
+char Rechenzeichenn = Convert.ToChar(Console.ReadLine());
+
+switch (Rechenzeichenn)
+{
+    case '+':
+        ergebnis = Zahl1 + Zahl2;
+        Console.WriteLine(ergebnis);
+        break;
+    case '-':
+        ergebnis = Zahl1 - Zahl2;
+        Console.WriteLine(ergebnis);
+        break;
+    case '*':
+        ergebnis = Zahl1 * Zahl2;
+        Console.WriteLine(ergebnis);
+        break;
+    case '/':
+        ergebnis = Zahl1 / Zahl2;
+        Console.WriteLine(ergebnis);
+        break;
+    case '%':
+        ergebnis = Zahl1 % Zahl2;
+        Console.WriteLine(ergebnis);
+        break;
+    case 'a':
+        ergebnis = Zahl1 + Zahl2;
+        Console.WriteLine(ergebnis);
+        ergebnis = Zahl1 * Zahl2;
+        Console.WriteLine(ergebnis);
+        ergebnis = Zahl1 - Zahl2;
+        Console.WriteLine(ergebnis);
+        ergebnis = Zahl1 % Zahl2;
+        Console.WriteLine(ergebnis);
+        break;
+    default:
+        ergebnis = 0;
+        break;
+}
